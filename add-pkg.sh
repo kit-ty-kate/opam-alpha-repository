24fe17e0d2fcbf90394b69e45bc70e2aabae90c0
Kate <kit.ty.kate@disroot.org>
2022-02-18 13:45:27 +0000

    Add atd* for OCaml 5

--- add-pkg.sh
+++ add-pkg.sh
@@ -2,16 +2,18 @@
 
 set -e
 
-if test "$#" != 1; then
+if test "$#" -lt 1; then
   echo "Usage: $0 PACKAGE"
   exit 1
 fi
 
-pkg=$1
-shift
+while test "$#" -ge 1; do
+  pkg=$1
+  shift
 
-last_version=$(opam show -f version "$pkg" | cut -d~ -f1)
-target=$pkg/$pkg.${last_version}.1~alpha-repo
+  last_version=$(opam show -f version "$pkg" | cut -d~ -f1)
+  target=$pkg/$pkg.${last_version}.1~alpha-repo
 
-mkdir -p "$target"
-opam show --raw "$pkg" > "$target/opam"
+  mkdir -p "$target"
+  opam show --raw "$pkg" > "$target/opam"
+done
